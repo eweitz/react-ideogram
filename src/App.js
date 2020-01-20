b18f6bd069b557290b1e5aae37022c4807b4df76
Eric Weitz <eric.m.weitz@gmail.com>
2020-01-20 15:56:06 -0500

    Add basic React example from eweitz/ideogram

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Ideogram from 'ideogram';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Ideogram in React!</h1>
+        </header>
+        <AppIdeogram/>
+      </div>
+    );
+  }
+}
+
+class AppIdeogram extends Component {
+
+  componentDidMount() {
+    return new Ideogram({
+      organism: 'human',
+      dataDir: 'https://unpkg.com/ideogram@1.16.0/dist/data/bands/native/',
+      container: '#ideo-container'
+    });
+  }
+
+  render() {
+    return (
+      <div id="ideo-container"></div>
+    );
+  }
 }
 
 export default App;
