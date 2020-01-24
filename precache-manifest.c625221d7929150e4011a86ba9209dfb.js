2d9647ea207340f21785087d429e2d650eb24535
Eric Weitz <eric.m.weitz@gmail.com>
2020-01-24 06:45:13 -0500

    Updates

---
+++ precache-manifest.c625221d7929150e4011a86ba9209dfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b204126e6ac00f5355ff16124c0505aa",
+    "url": "/ideogram-react/index.html"
+  },
+  {
+    "revision": "ff2e44d2cf4896a9c8f4",
+    "url": "/ideogram-react/static/css/main.034a75f0.chunk.css"
+  },
+  {
+    "revision": "1a768f213acfb5c57b24",
+    "url": "/ideogram-react/static/js/2.c8983019.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ideogram-react/static/js/2.c8983019.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff2e44d2cf4896a9c8f4",
+    "url": "/ideogram-react/static/js/main.c4a39713.chunk.js"
+  },
+  {
+    "revision": "4d7079916ad3df30bad7",
+    "url": "/ideogram-react/static/js/runtime-main.a9e8531b.js"
+  }
+]);
