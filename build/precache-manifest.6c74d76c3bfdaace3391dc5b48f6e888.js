6374eb1a01d24725fccd5b50ef43c7cddd06d33a
Eric Weitz <eric.m.weitz@gmail.com>
2020-01-26 16:31:31 -0500

    Debug, restore build in master source

---
+++ build/precache-manifest.6c74d76c3bfdaace3391dc5b48f6e888.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e14542721387b6a72cd88b86c3f797",
+    "url": "/react-ideogram/index.html"
+  },
+  {
+    "revision": "9ac43012ba9d65978e11",
+    "url": "/react-ideogram/static/css/main.034a75f0.chunk.css"
+  },
+  {
+    "revision": "8deb1a4d0de6d0c30bc3",
+    "url": "/react-ideogram/static/js/2.07499d52.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-ideogram/static/js/2.07499d52.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ac43012ba9d65978e11",
+    "url": "/react-ideogram/static/js/main.d8b8e420.chunk.js"
+  },
+  {
+    "revision": "41ea1b6963340ae7dacb",
+    "url": "/react-ideogram/static/js/runtime-main.50c62243.js"
+  }
+]);
